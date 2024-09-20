069af37caed8581f3bb9a2af5cabf6974c380957
Alexandr Shcherbatyuk <a.scherbatyuk@gmail.com>
2024-09-21 00:07:24 +0800

    Added wagmi + walletconnect

--- App.tsx
+++ App.tsx
@@ -1,14 +1,41 @@
+import '@walletconnect/react-native-compat'
+import { WagmiProvider } from 'wagmi'
+import { sepolia } from '@wagmi/core/chains' //mainnet, polygon, arbitrum
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createAppKit, defaultWagmiConfig, AppKit, AppKitButton } from '@reown/appkit-wagmi-react-native'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// 0. Setup queryClient
+const queryClient = new QueryClient()
+
+// 1. Get projectId at https://cloud.reown.com
+const projectId = '4c3ab43569b1c52cabc36673903c0732'
+
+// 2. Create config
+const metadata = {
+  name: 'AppKit RN',
+  description: 'AppKit RN Example',
+  url: 'https://reown.com/appkit',
+  icons: ['https://avatars.githubusercontent.com/u/179229932'],
+  redirect: {
+    native: 'native-app://',
+    universal: 'http://native-app-test.com'
+  }
 }
+
+const chains = [sepolia] as const //mainnet, polygon, arbitrum
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata }) //
+
+// 3. Create modal
+createAppKit({
+  projectId,
+  wagmiConfig,
+  defaultChain: sepolia, // Optional
+  enableAnalytics: true // Optional - defaults to your Cloud configuration,
+})
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +45,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function App() {
+
+return (
+  <WagmiProvider config={wagmiConfig}>
+    <View style={styles.container}>
+    <QueryClientProvider client={queryClient}>
+      <AppKitButton />
+      
+      <StatusBar style="auto" />
+        <AppKit />
+      </QueryClientProvider>
+    </View>
+  </WagmiProvider>
+);
+}
