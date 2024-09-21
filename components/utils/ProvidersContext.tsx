44379ea93a789cd8c1135cd488725b14a24e0f6b
asimaranov <a.simaranov@gmail.com>
2024-09-22 07:40:16 +0800

    Added tx making

--- components/utils/ProvidersContext.tsx
+++ components/utils/ProvidersContext.tsx
@@ -1,47 +1,20 @@
 import {
   AppKit,
   createAppKit,
-  defaultWagmiConfig,
 } from "@reown/appkit-wagmi-react-native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
-import { mainnet, sepolia } from "viem/chains";
+import { mainnet } from "viem/chains";
 import { WagmiProvider } from "wagmi";
-import { authConnector } from "@reown/appkit-auth-wagmi-react-native";
-import { dynamicClient } from "../../utils/dynamic";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-
-type Props = {
-  children: React.ReactNode;
-};
+import { projectId, wagmiConfig } from "./wagmi";
 
 // 0. Setup queryClient
 const queryClient = new QueryClient();
 
-// 1. Get projectId at https://cloud.reown.com
-const projectId = "b1e1aefc0165086def75803b4e1cda7e";
-
-// 2. Create config
-const metadata = {
-  name: "AppKit RN",
-  description: "AppKit RN Example",
-  url: "https://reown.com/appkit",
-  icons: ["https://avatars.githubusercontent.com/u/179229932"],
-  redirect: {
-    native: "native-app://",
-    universal: "http://native-app-test.com",
-  },
+type Props = {
+  children: React.ReactNode;
 };
-
-const chains = [mainnet, sepolia] as const; //mainnet, polygon, arbitrum
-
-const auth = authConnector({ projectId, metadata });
-
-const wagmiConfig = defaultWagmiConfig({
-  chains,
-  projectId,
-  metadata,
-});
 
 // 3. Create modal
 createAppKit({
