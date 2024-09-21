82d3dc6c904b20fb2553f358c554a35b99200733
Neo-Gar <nvatlecov@mail.ru>
2024-09-21 16:52:40 +0800

    Added GroupsItem

---
+++ components/GroupsItem.tsx
@@ -0,0 +1,117 @@
+import React from "react";
+import { Pressable, View, Text, Image } from "react-native";
+
+export default function GroupsItem({
+  id,
+  groupName,
+  owned,
+  owe,
+  image,
+}: {
+  id: string;
+  groupName: string;
+  owned: number;
+  owe: number;
+  image?: any;
+}) {
+  return (
+    <Pressable
+      style={{
+        backgroundColor: "#EEEEEF",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%",
+        padding: 5,
+        borderRadius: 5,
+        gap: 10,
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "#E1E1E3",
+          borderRadius: 5,
+          width: 62,
+          height: 62,
+        }}
+      />
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Text style={{ fontSize: 18, fontWeight: "500", marginBottom: 5 }}>
+          {groupName}
+        </Text>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: 5,
+            marginBottom: 3,
+          }}
+        >
+          <Text style={{ fontSize: 14, color: "#00C74F" }}>You are owed:</Text>
+          <Text style={{ fontSize: 14, color: "#00C74F" }}>{owned}$</Text>
+        </View>
+        <View style={{ display: "flex", flexDirection: "row", gap: 5 }}>
+          <Text style={{ fontSize: 14, color: "#FF0000" }}>You owe:</Text>
+          <Text style={{ fontSize: 14, color: "#FF0000" }}>{owe}$</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+          height: "100%",
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 5,
+          }}
+        >
+          <Pressable
+            style={{
+              backgroundColor: "#2F28D0",
+              paddingVertical: 6,
+              paddingHorizontal: 9,
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: 5,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 14,
+                color: "white",
+              }}
+            >
+              SETTLE UP
+            </Text>
+          </Pressable>
+          <Pressable
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "#D9D9D9",
+              padding: 7,
+              borderRadius: 5,
+            }}
+          >
+            <Image
+              source={require("../assets/misc/trash.png")}
+              style={{
+                width: 10,
+                height: 12,
+              }}
+            />
+          </Pressable>
+        </View>
+      </View>
+    </Pressable>
+  );
+}
--- components/screens/Groups.tsx
+++ components/screens/Groups.tsx
@@ -1,21 +1,88 @@
 import React from "react";
-import { SafeAreaView, View, Image } from "react-native";
+import { SafeAreaView, View, Image, Text, ScrollView } from "react-native";
+import GroupsItem from "../GroupsItem";
 
 export default function Groups() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Image
+        source={require("../../assets/logo-little.png")}
+        style={{
+          width: 117,
+          height: 31,
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+          marginVertical: 35,
+          marginLeft: 10,
+        }}
+      />
       <View
         style={{
           flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <Image
-          source={require("../../assets/logo-little.png")}
+        <Text
           style={{
-            width: 117,
-            height: 31,
+            fontSize: 35,
+            fontWeight: 500,
+            marginBottom: 12,
+            width: "90%",
           }}
-        />
+        >
+          GROUPS
+        </Text>
+        <ScrollView
+          style={{
+            flex: 2,
+            width: "95%",
+            display: "flex",
+            flexDirection: "column",
+            paddingVertical: 10,
+          }}
+          contentContainerStyle={{
+            rowGap: 10,
+          }}
+          showsVerticalScrollIndicator={false}
+        >
+          <GroupsItem
+            id={"somegroup"}
+            groupName={"Group Name 1"}
+            owned={500}
+            owe={1500.22}
+          />
+          <GroupsItem
+            id={"somegroup"}
+            groupName={"Group Name 1"}
+            owned={500}
+            owe={1500.22}
+          />
+          <GroupsItem
+            id={"somegroup"}
+            groupName={"Group Name 1"}
+            owned={500}
+            owe={1500.22}
+          />
+          <GroupsItem
+            id={"somegroup"}
+            groupName={"Group Name 1"}
+            owned={500}
+            owe={1500.22}
+          />
+          <GroupsItem
+            id={"somegroup"}
+            groupName={"Group Name 1"}
+            owned={500}
+            owe={1500.22}
+          />
+          <GroupsItem
+            id={"somegroup"}
+            groupName={"Group Name 1"}
+            owned={500}
+            owe={1500.22}
+          />
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
